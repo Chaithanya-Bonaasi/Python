0c4bc3ecec6b5d4a19e703ce38f53d3209a19151
Chaithanya-Bonaasi <chaitanyabonaasi@gmail.com>
2026-07-25 20:23:32 +0530

    init commit

---
+++ string methods.py
@@ -0,0 +1,36 @@
+name="akshaya"
+print(name.upper())
+name="AKSHAYA"
+print(name.lower())
+heading="i love python"
+print(heading.title())
+name="python"
+print(name.capitalize())
+name="python"
+print(len(name))
+text="i love python"
+print(text.replace("java","python"))
+text="python"
+print(text.find("t"))
+text="banana"
+print(text.count("b"))
+text="i love python"
+print(text.split())
+text="mango,apple,banana"
+print(text.split())
+text="A,B,C"
+print(text.split(","))
+text="A,B,C"
+print(text.split())
+text="Python SQL Java"
+print(text.split())
+text="Akshaya is learning python"
+print(text.split())
+text=" python "
+print(text.strip())
+text=" Hello"
+print(text.lstrip())
+text="world "
+print(text.rstrip())
+text=" i love python "
+print(text.strip())
